0e3d57f5eb552b66ee1ed1b462b30480c3b3a1dc
Derine Banis <d.banis.13@gmail.com>
2022-11-18 15:47:49 -0800

    Unclicked buttons change color when 8 other buttons are pressed. Simulating incoming disabled feature.

--- components/BeatButton.jsx
+++ components/BeatButton.jsx
@@ -7,13 +7,16 @@ export const BeatButton = (props) => {
   const sound = props.sound //change to props.sound
   const icon = '/images/test.gif' // change to props.icon
   const label = props.label
+  const activeCount = props.activeCount
+  const setActiveCount = props.setActiveCount
   
   const [play, exposedData] = useSound(sound, {loop: true});
   const [playing, setPlaying] = useState(false)
 
   const buttonClass = classNames({
     'bg-slate-400 text-white p-2 m-2 w-40 h-40': true,
-    'bg-pink-500' : playing
+    'bg-pink-500' : playing,
+    'bg-yellow-500': activeCount >= 8 && !playing,
 
 })
   
@@ -26,11 +29,14 @@ export const BeatButton = (props) => {
         if(!playing){
           setPlaying(true)
           play()
+          setActiveCount(activeCount+1)
         }
         else{
           setPlaying(false)
           exposedData.stop()
+          setActiveCount(activeCount-1)
         }
+        console.log(activeCount)
       }}
     >
     {/* <h1>{label}</h1> */}
--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,27 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { BeatButton } from '../components/BeatButton';
 import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react';
 
 //All sounds from sounds public folder
 import {sounds} from '../public/sounds/index'
 
+
 export default function Home() {
+  const [activeCount, setActiveCount] = useState(0)
+  // useEffect(()=>{
+  //   console.log('yo')
+    
+  // },[activeCount])
   return (
     <div className={styles.container}>
       {sounds.map((sound, index)=>{
         return(
           <BeatButton
-        
             sound={sound}
             label={`Button ${index}`}
+            activeCount={activeCount}
+            setActiveCount={setActiveCount}
           />
         )
       })}
